7dd117891b90172a6f28d8fcd00f262e69f0b3b5
Constantjunior <junzgamos@gmail.com>
2020-11-06 15:20:41 +0100

    last commit

---
+++ data_car.py
@@ -0,0 +1,45 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+
+app = dash.Dash()
+
+df = pd.read_csv(
+    'carData.csv')
+
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x= df['year']
+                    y=df['selling_price']
+
+                    text=df[df['Present_Price'] == i]['Present_Price'],
+                    mode='markers',
+                    opacity=0.8,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in range(len(x)):
+                    xycovariance += (x[i] - xmean) * (y[i] - ymean)
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(host="localhost", port=8001, debug=True)
